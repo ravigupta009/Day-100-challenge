d62dd575e193055d9b667cbb28be32fd6c738d52
Ravi Gupta <ravi720904@gmail.com>
2025-11-17 19:19:55 +0530

    Add files via upload

---
+++ Day6_p11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter your number = ");
+    scanf("%d",num);
+    if (num % 2 == 0)
+    {
+        printf("odd order");
+    }
+    else
+    {
+        printf("even order");
+    }
+
+}

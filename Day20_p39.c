5feecf49907ab3cb9448cc84fcbdd2a30251c620
Ravi Gupta <ravi720904@gmail.com>
2025-11-20 00:25:36 +0530

    Add files via upload

---
+++ Day20_p39.c
@@ -0,0 +1,10 @@
+/*Write a program to find the product of odd digits of a number.*/
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter odd number =");
+    scanf("%d",&n);
+    
+    return 0;
+}

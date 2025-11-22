b5b930028f24ec27c0042c05384b0dbd6eb674f7
Ravi Gupta <ravi720904@gmail.com>
2025-11-22 12:21:46 +0530

    Add files via upload

---
+++ Day28_p56.c
@@ -0,0 +1,23 @@
+/*Read and print elements of a one-dimensional array.*/
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+   
+    printf("You entered:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}

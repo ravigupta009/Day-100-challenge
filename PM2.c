ca4a4a8e98e9c61da39e5b9c4f48807b752e2bd3
Ravi Gupta <ravi720904@gmail.com>
2026-01-08 19:33:04 +0530

    Add files via upload

---
+++ PM2.c
@@ -0,0 +1,68 @@
+// #include <stdio.h>
+
+// int main(){
+//     printf("ravi");
+//     int n;
+//     n = 5;
+//     n = 7;
+//     printf("%d",n);
+//     return 0;
+// }
+
+// 
+
+
+
+// #include <stdio.h>
+
+// int main(){
+//     int n;
+//     n = 7;
+//     printf("%d\n",n);
+//     n = n + 19;
+//     printf("%d",n);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main(){
+//     int x, y;
+//     x = 8;
+//     y = 6;
+//     printf("add = %d\n", x+y);
+//     printf("sub  = %d\n",x-y);
+//     printf("Mult = %d\n",x*y);
+//     printf("div = %d\n",x/y);
+
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main(){
+//     float x;
+//     x = 3.14;
+//     printf("%f\n",x);
+//     float a, y;
+//     a = 9;
+//     y = 4;
+//     printf("%f",a/y);    
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+
+// int main(){
+//     int r = 5;
+//     float  x = 4*3.1415*r*r*r/3;
+
+
+//     printf("volume of sphare = %f",x);
+//     return 0;
+// }
+
